0424fe2edb053bfc1d5b429fc2a3d7933fe8221e
gurjung <gurjung997@gmail.com>
2025-05-18 23:23:03 +0530

    feat(array): implement removeDuplicates using two-pointer approach

---
+++ arrays/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    
+    let left = 0;
+
+    for(let right = 1; right < nums.length; right++) {
+        if(nums[left] !== nums[right]) {
+            left++;
+            nums[left] = nums[right];
+        }
+    }
+
+    return left + 1;
+};
