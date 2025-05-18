1f1e295eb4758c0bf3d7532519cd53f26ab80a6b
gurjung <gurjung997@gmail.com>
2025-05-19 00:45:42 +0530

    fix: formatting

--- arrays/remove-duplicates-from-sorted-array.js
+++ arrays/remove-duplicates-from-sorted-array.js
@@ -1,14 +1,14 @@
-var removeDuplicates = function(nums) {
-    if (nums.length === 0) return 0;
-    
-    let left = 0;
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
 
-    for(let right = 1; right < nums.length; right++) {
-        if(nums[left] !== nums[right]) {
-            left++;
-            nums[left] = nums[right];
-        }
+  let left = 0;
+
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[left] !== nums[right]) {
+      left++;
+      nums[left] = nums[right];
     }
+  }
 
-    return left + 1;
+  return left + 1;
 };
--- objects/two-sum.js
+++ objects/two-sum.js
@@ -1,6 +1,6 @@
 // BRUTE FORCE
 
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
   for (let left = 0; left < nums.length; left++) {
     for (let right = left + 1; right < nums.length; right++) {
       if (nums[left] + nums[right] === target) {
@@ -13,20 +13,19 @@ var twoSum = function(nums, target) {
 
 // HASH MAP APPROACH
 
-var twoSum = function(nums, target) {
-    const obj = {};
-    
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        
-        if (complement in obj) {
-            return [obj[complement], i];
-        }
-        
-        obj[nums[i]] = i;
-    }
-};
+var twoSum = function (nums, target) {
+  const obj = {};
 
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (complement in obj) {
+      return [obj[complement], i];
+    }
+
+    obj[nums[i]] = i;
+  }
+};
 
 // USING MAP DATA SET
 // TODO
